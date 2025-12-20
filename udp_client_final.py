f94ce4e26eaa032ce6eff4fd5517d44b1d011bca
Marckson Icomp <marcksonms@icomp.ufam.edu.br>
2025-12-20 17:11:56 -0400

    mudança tempo de espera para primeiro processamento

--- udp_client_final.py
+++ udp_client_final.py
@@ -17,8 +17,8 @@ import time
 SERVER_IP = "10.0.0.1"
 SERVER_PORT = 4433
 
-TIMEOUT = 1.0       # timeout para esperar ACK
-MAX_RETRIES = 3     # retransmissões por pacote
+TIMEOUT = 0.0       # timeout para esperar ACK
+MAX_RETRIES = 5     # retransmissões por pacote
 NUM_DATA_PKTS = 5   # quantos pacotes de dados enviar
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
